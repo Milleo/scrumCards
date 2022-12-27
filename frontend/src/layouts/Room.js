10e59937dc7da30c7a74f052116ccf9e5b971946
Rafael Milléo <rafael@milleo.net>
2022-12-27 16:42:50 -0300

    Tests with socket io

--- frontend/src/layouts/Room.js
+++ frontend/src/layouts/Room.js
@@ -1,24 +1,40 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Card } from "../components";
-import { useParams } from "react-router-dom"
+import { useParams, useHistory } from "react-router-dom"
 import axios from "axios";
-import { useHistory } from "react-router-dom";
+import io from 'socket.io-client';
 
 function Room(props){
     const {roomURI} = useParams();
     const history = useHistory();
     const [ loading, setLoading ] = useState(false);
     const [ roomData, setRoomData ] = useState({});
+    const socket = io();
+    const [ userCount, setUserCount ] = useState(0);
 
-    axios.get(`/api/rooms/${roomURI}`).then((res) => {
-        setLoading(false);
-        console.log(res);
-        setRoomData(res.data);
-    }).catch(() => {
-        history.push("/404");
-    })
+    useEffect(() => {
+        axios.get(`/api/rooms/${roomURI}`).then((res) => {
+            setLoading(false);
+            setRoomData(res.data);
+        }).catch(() => {
+            history.push("/404");
+        });
+
+        socket.on("welcome", (data) => {
+            console.log(data);
+        })
+
+        return () => {
+            socket.off('TESTE');
+            socket.off('welcome');
+            socket.off('connect');
+            socket.off('disconnect');
+        };
+    }, []);
+    
 
     return <Fragment><h1>{roomData.name}</h1>
+        {userCount}
         <Card />
     </Fragment>
 }

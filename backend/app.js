43d54754c599acbffe9a2dac528e3d00514ad34b
Rafael Milléo <rafael@milleo.net>
2022-12-27 16:40:09 -0300

    Adding socket io to backend

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,33 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
+var http = require('http');
 
 var indexRouter = require('./routes/index');
 var roomsRouter = require("./routes/RoomsRouter");
 var usersRouter = require("./routes/UsersRouter");
 
 var app = express();
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+ function normalizePort(val) {
+    var port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+  
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+  
+    return false;
+  }
 
 app.use(cors())
 app.use(logger('dev'));
@@ -21,4 +42,28 @@ app.use('/', indexRouter);
 app.use('/rooms', roomsRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+var port = normalizePort(process.env.PORT || '3001');
+app.set('port', port);
+
+var server = http.createServer(app);
+var { Server } = require("socket.io");
+var io = new Server(server);
+var userCount = 0;
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.join("AAAAAAAAAA");
+    const roomCount = io.sockets.adapter.rooms.get("AAAAAAAAAA").size
+    io.emit("welcome", roomCount);
+
+    socket.on("TESTE", () => {
+        console.log("TESTE FIRED");
+        io.emit("TESTE", userCount++);
+    })
+
+    socket.on("disconnect", () => {
+        io.emit("TESTE", userCount--);
+    })
+});
+
+module.exports = [server, port];

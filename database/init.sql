85d13c16f66ec537c08200fe760fcd2cdd998428
Rafael Milléo <rafael@milleo.net>
2022-12-30 16:42:34 -0300

    Temporaly allowing user to do anything in DB

--- database/init.sql
+++ database/init.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE IF NOT EXISTS scrum_cards;
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, REFERENCES ON scrum_cards.* TO 'scrum_cards_user_1'@'%' WITH GRANT OPTION;
+/* @TODO Make the proper GRANT for this user
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, REFERENCES ON scrum_cards.* TO 'scrum_cards_user_1'@'%' WITH GRANT OPTION; */
+/GRANT ALL ON scrum_cards.* TO 'scrum_cards_user_1'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;

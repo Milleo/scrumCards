027d0edae143ffbdf5a9b902da9900377d22d0f5
Rafael Milléo <rafael@milleo.net>
2023-01-06 12:54:21 -0300

    Adding onBlur and changing coding layout

--- frontend/src/components/FormField.js
+++ frontend/src/components/FormField.js
@@ -1,12 +1,20 @@
 import { Form } from "react-bootstrap";
 
 const FormField = (props) => {
-    const { errors, label, name, onChange, password, values } = props;
+    const { errors, label, name, onBlur, onChange, password, touched, values } = props;
     let fieldType = password?"password":"text";
     
     return <Form.Group className="mb-3">
         <Form.Label htmlFor={name}>{label}</Form.Label>
-        <Form.Control id={name} aria-label={name} isInvalid={!!errors[name]} name={name} value={values[name]} onChange={onChange} type={fieldType} />
+        <Form.Control
+            id={name}
+            aria-label={name}
+            isInvalid={!!errors[name] && !!touched[name]}
+            name={name}
+            value={values[name]}
+            onBlur={onBlur}
+            onChange={onChange}
+            type={fieldType} />
         <Form.Control.Feedback type="invalid">{errors[name]}</Form.Control.Feedback>
     </Form.Group>
 }

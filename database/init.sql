15c1b725df3088f6fbed69b9600dea5f69626356
Rafael Milléo <rafael@milleo.net>
2023-01-06 09:47:59 -0300

    Removing invalid char

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS scrum_cards;
 /* @TODO Make the proper GRANT for this user
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, REFERENCES ON scrum_cards.* TO 'scrum_cards_user_1'@'%' WITH GRANT OPTION; */
-/GRANT ALL ON scrum_cards.* TO 'scrum_cards_user_1'@'%' WITH GRANT OPTION;
+GRANT ALL ON scrum_cards.* TO 'scrum_cards_user_1'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;

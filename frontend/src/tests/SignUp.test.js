bd583322268b67974fc988f18eff0562c79030b7
Rafael Milléo <rafael@milleo.net>
2023-01-06 13:03:56 -0300

    Fixing validation

--- frontend/src/tests/SignUp.test.js
+++ frontend/src/tests/SignUp.test.js
@@ -169,16 +169,18 @@ describe("Sing up page tests", () => {
             email: "jest_user@gmail.com",
             password: "SomeStrongPassword#!123"
         }
-        const { getByRole, getByLabelText } = render(<SignUp />);
+        const { getByRole, getByLabelText, getByText } = render(<SignUp />);
         const submitButton = getByRole("button", { name: "Sign up" });
         const feedback = getByRole("textbox", { name: "email" }).parentElement.querySelector(".invalid-feedback");
-
-        await userEvent.type(getByRole("textbox", { name: "name" }), formPayload.name);
-        await userEvent.type(getByRole("textbox", { name: "userName" }), formPayload.userName);
-        await userEvent.type(getByRole("textbox", { name: "email" }), formPayload.email);
-        await userEvent.type(getByLabelText("Password"), formPayload.password);
-        await userEvent.type(getByLabelText("Password confirmation"), formPayload.password);
-        userEvent.click(submitButton);
+        await waitFor(async () => {
+            await userEvent.type(getByRole("textbox", { name: "name" }), formPayload.name);
+            await userEvent.type(getByRole("textbox", { name: "userName" }), formPayload.userName);
+            await userEvent.type(getByRole("textbox", { name: "email" }), formPayload.email);
+            await userEvent.type(getByLabelText("Password"), formPayload.password);
+            await userEvent.type(getByLabelText("Password confirmation"), formPayload.password);
+            userEvent.click(submitButton);
+        })
+        
 
         await waitFor(() => {
             expect(history.location.pathname).toEqual('/');
@@ -195,13 +197,15 @@ describe("Sing up page tests", () => {
         const { getByRole, getByLabelText } = render(<SignUp />);
         const submitButton = getByRole("button", { name: "Sign up" });
         const feedback = getByRole("textbox", { name: "userName" }).parentElement.querySelector(".invalid-feedback");
-
-        await userEvent.type(getByRole("textbox", { name: "name" }), formPayload.name);
-        await userEvent.type(getByRole("textbox", { name: "userName" }), formPayload.userName);
-        await userEvent.type(getByRole("textbox", { name: "email" }), formPayload.email);
-        await userEvent.type(getByLabelText("Password"), formPayload.password);
-        await userEvent.type(getByLabelText("Password confirmation"), formPayload.password);
-        userEvent.click(submitButton);
+        
+        await waitFor(async () => {
+            await userEvent.type(getByRole("textbox", { name: "name" }), formPayload.name);
+            await userEvent.type(getByRole("textbox", { name: "userName" }), formPayload.userName);
+            await userEvent.type(getByRole("textbox", { name: "email" }), formPayload.email);
+            await userEvent.type(getByLabelText("Password"), formPayload.password);
+            await userEvent.type(getByLabelText("Password confirmation"), formPayload.password);
+            userEvent.click(submitButton);
+        });
 
         await waitFor(() => {
             expect(history.location.pathname).toEqual('/');

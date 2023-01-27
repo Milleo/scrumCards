58b47be3a769d54615abcc0afff35f4a736a1dd3
Rafael Milléo <rafael@milleo.net>
2023-01-27 15:08:43 -0300

    Removing socketio calls temporaly

--- frontend/src/pages/Room.js
+++ frontend/src/pages/Room.js
@@ -4,12 +4,11 @@ import { useParams, useHistory } from "react-router-dom";
 import axios from "axios";
 import io from 'socket.io-client';
 
-function Room(props){
+function Room(){
     const {roomURI} = useParams();
     const history = useHistory();
     const [ loading, setLoading ] = useState(false);
     const [ roomData, setRoomData ] = useState({});
-    const socket = io();
     const [ userCount, setUserCount ] = useState(0);
 
     useEffect(() => {
@@ -21,17 +20,6 @@ function Room(props){
                 history.push("/404");
             });
         }
-
-        socket.on("welcome", (data) => {
-            console.log(data);
-        })
-
-        return () => {
-            socket.off('TESTE');
-            socket.off('welcome');
-            socket.off('connect');
-            socket.off('disconnect');
-        };
     }, []);
     
 

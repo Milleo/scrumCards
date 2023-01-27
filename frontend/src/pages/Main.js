afdfc53fbd45fb9df8a2d234f05ae39c6da2ca28
Rafael Milléo <rafael@milleo.net>
2023-01-27 12:30:50 -0300

    UPdating frontend

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,14 +1,16 @@
-import Row from 'react-bootstrap/Row';
+import { Col, Row } from 'react-bootstrap';
 import { FormattedMessage } from "react-intl";
 import { Link } from "react-router-dom";
 
 
 function Main(){
     return(
-        <Row className="align-self-center" style={{ marginTop: "50px" }}>
-            <h2><FormattedMessage id='main.title' /></h2>
-            <p><FormattedMessage id='main.welcomeMessage' /></p>
-            <Link className="btn btn-primary" to="/newRoom">Criar uma sala</Link>
+        <Row className="align-self-center">
+            <Col>
+                <h2><FormattedMessage id='main.title' /></h2>
+                <p><FormattedMessage id='main.welcomeMessage' /></p>
+                <Link className="btn btn-primary" to="/newRoom">Criar uma sala</Link>
+            </Col>
         </Row>
     )
 }
